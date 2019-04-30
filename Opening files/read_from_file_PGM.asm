708413b1470e8cb6a9c1719bdbacc82426fcb5b3
Lucas Pacheco <lucas.pacheco9@hotmail.com>
2019-04-30 16:26:24 -0300

    ata

--- Opening files/read_from_file_PGM.asm
+++ Opening files/read_from_file_PGM.asm
@@ -1,31 +1,34 @@
 .data
 finput:	.asciiz "read.pgm"
 foutput:	.asciiz "write.pgm"
-space:	.space 18
+space:	.space 1
 end:
 .text
 
-# Abrindo arquivo para leitura 
-li $v0, 13
-la $a0, finput
-li $a1, 0
-li $a2, 0
-syscall
-move $s6, $v0
+OpenRead:
+	# Abrindo arquivo para leitura 
+	li $v0, 13
+	la $a0, finput
+	li $a1, 0	
+	li $a2, 0
+	syscall
+	move $s6, $v0
 
+ReadFile:
+	# Lendo o arquivo
+	li $v0, 14	
+	move $a0, $s6
+	la $a1, space
+	li $a2, 1
+	syscall
 
-# Lendo o arquivo
-li $v0, 14
-move $a0, $s6
-la $a1, space
-li $a2, 18
-syscall
-
+CloseFile:
 # Fechar arquivo
 li $v0, 16		#syscall para fechar arquivo
 move $a0, $s6	# arquivo que vai fechar
 syscall
 
+OpenWrite:
 # Abrindo arquivo para escrita 
 li $v0, 13
 la $a0, foutput
@@ -34,15 +37,10 @@ li $a2, 0
 syscall
 move $s6, $v0
 
-#lb $t1, space
-
+WriteFile:
 li $v0, 15		# syscall para escrita no arquivo
 move $a0, $s6	# movendo arquivo p/  $a0
 la $a1, space	# o que vai ser escrito
-li $a2, 18		# tamanho do buffer
+li $a2, 1		# tamanho do buffer
 syscall
 
-# Fechar arquivo
-li $v0, 16		#syscall para fechar arquivo
-move $a0, $s6	# arquivo que vai fechar
-syscall
